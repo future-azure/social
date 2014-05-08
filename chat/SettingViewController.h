6cf01b3e22e2bb6a44269bf7b3ee955eeda7f966
guwt <guwt@brightvision.com.cn>
2014-05-08 13:50:45 +0800

    settingView初始化

---
+++ chat/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  chat
+//
+//  Created by brightvision on 14-5-6.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
